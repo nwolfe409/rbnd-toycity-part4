f11ccdc8f188a02fc776024de1254d40e3d1c212
Nathan Wolfe <nwolfe409@gmail.com>
2016-03-06 21:35:56 +0000

    correct seed.rb to properly seed the database for testing

--- data/seeds.rb
+++ data/seeds.rb
@@ -1,8 +1,15 @@
 require 'faker'
+require 'csv'
 
 # This file contains code that populates the database with
 # fake data for testing purposes
+@data_path = File.dirname(__FILE__) + "/../data/data.csv"
 
 def db_seed
-  # Your code goes here!
+  CSV.open(@data_path, "ab") do |csv|
+    10.times do |i|
+      csv << [i, Faker::Commerce.department, Faker::Commerce.product_name, Faker::Number.decimal(2)]
+    end
+  end
 end
+
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -2,24 +2,25 @@ require_relative 'find_by'
 require_relative 'errors'
 require 'csv'
 
+@@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
 class Udacidata
   
-  @@data_path = File.dirname(__FILE__) + "/../data/data.csv" 
+   
+  
   
   def self.create(attributes = nil)
-  
-    object = self.new(attributes) # Initialize new product # create the object
-
-    CSV.foreach(@@data_path) do |row|
-      if !row.include?(object.name) # If the object's data is not in the database
-        CSV.open(@@data_path, "a+") do |csv| # save the data in the database
-          csv << [object.id, object.brand, object.name, object.price]
-        end
+    File.open(@@data_path, 'ab+') do |this_file|
+      csv_data_ids = CSV.read(this_file).map{|row| row.first.to_i}
+      csv_data_ids.shift
+      unless csv_data_ids.include?(attributes[:id])
+        CSV.open(this_file, "ab") {|csv| csv << [csv_data_ids.length.to_s, attributes[:brand].to_s, attributes[:name].to_s, attributes[:price].to_s]} 
+        attributes[:id] = csv_data_ids.length.to_s
       end
+      return_object = self.new(attributes)
     end
-
-    object # return the object
-
-  end 
+  end
+      
+    
   
 end
